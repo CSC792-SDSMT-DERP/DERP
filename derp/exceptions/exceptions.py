86b65dcbcb4e85d4ad0e16bf7ba7e4091fc0f45e
Andrew Stelter <andrew@stelterfamily.net>
2018-11-18 15:44:32 -0700

    Base DerpException exception type

--- derp/exceptions/exceptions.py
+++ derp/exceptions/exceptions.py
@@ -1,43 +1,48 @@
-class IOException (Exception):
+class DerpException(Exception):
+    """ Base class of Derp exceptions """
+    pass
+
+
+class IOException (DerpException):
     """ Indicates that reading or writing files failed"""
     pass
 
 
-class TextParseException (Exception):
+class TextParseException (DerpException):
     """ Indicates that parsing using the Lark parser failed """
     pass
 
 
-class GrammarMergeException (Exception):
+class GrammarMergeException (DerpException):
     """ Indicates that merging two Grammars is not possible """
     pass
 
 
-class GrammarDefinitionException (Exception):
+class GrammarDefinitionException (DerpException):
     """ Indicates that creating a Grammar from a dictionary is not possible """
     pass
 
 
-class SemanticException (Exception):
+class SemanticException (DerpException):
     """ Indicates that a processed AST is not semantically correct """
     pass
 
 
-class ModuleDefinitionException(Exception):
+class ModuleDefinitionException(DerpException):
     """ Indicates that a module was not able to be registered """
     pass
 
 
-class ModuleRegistrationException(Exception):
+class ModuleRegistrationException(DerpException):
     """ Indicates that a module was not able to be registered """
     pass
 
 
-class ModuleNotRegisteredException(Exception):
+class ModuleNotRegisteredException(DerpException):
     """ Indicates that a module was not registered when it was referenced """
     pass
 
 
-class ModuleNotLoadedException(Exception):
+class ModuleNotLoadedException(DerpException):
     """ Indicates that a module was not loaded when it was referenced """
     pass
