454785ac4f393d93215ecdcfb57a17e54af9065c
Andrew Stelter <andrew@stelterfamily.net>
2018-11-18 15:45:00 -0700

    Added comments to IModule for grammar rules

--- derp/modules/IModule.py
+++ derp/modules/IModule.py
@@ -14,12 +14,22 @@ class IModule(IPostQueryable):
     posts much match.
     """
 
+    # Names must be unique when lower case
     def name(self):
         """
         :return: the name of the module
         """
         pass
 
+    # Every rule or token must be prefixed with the module name to prevent rule name collisions
+    # The only things this grammar may rely on are the following:
+    # * The common lark ESCAPED_STRING is part of the grammar
+    # * Whitespace will be ignored when parsing
+    #
+    # One rule must be [module name]_source, this is the rule that's matched
+    #   to know when this module is to be used for a query
+    # That rule must not begin with _ or ?. It may begin with !. It may be a token (all caps),
+    #   or a rule (all lower-case)
     def source_grammar(self):
         """
         :return: the grammar for parsing sources interpreted by this module.
@@ -31,4 +41,3 @@ class IModule(IPostQueryable):
         :return: the PostDefinition which all posts returned from get_posts must match.
         """
         pass
-
