76c45787a0d834748663833e25c9fa08fec19bf7
Benjamin Garcia <benjamin.garcia@mines.sdsmt.edu>
2018-11-18 13:54:10 -0700

    fix PostMuxer and add IPostIterator

--- derp/session/selection_execution/PostIteratorMuxer.py
+++ derp/session/selection_execution/PostIteratorMuxer.py
@@ -11,16 +11,15 @@ class PostIteratorMuxer:
     This is used to provide a single iterator to read from for the PostReader.
     """
 
-    def __init__(self, sources=None):
-        self.__sources = sources if sources else []
+    def __init__(self, sources):
+        self.__sources = sources
 
     def __iter__(self):
         self.__curr_iter = 0
+        return self
 
     def __next__(self):
-        value = self.__sources[self.__curr_iter]
+        next_iter = self.__sources[self.__curr_iter]
         self.__curr_iter += 1
-        self.__curr_iter = self.__curr_iter == len(self.__sources) if 0 else self.__curr_iter
-
-    def add_source_iter(self, source_iter):
-        self.__sources.append(source_iter)
+        self.__curr_iter = self.__curr_iter % len(self.__sources)
+        return next(next_iter)
