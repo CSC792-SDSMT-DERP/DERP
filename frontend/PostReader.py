ac0f1ce9005c5d64be9d086a50e3a9ad6d847cf6
Logan Lembke <logan.lembke@mines.sdsmt.edu>
2018-11-25 21:32:31 -0700

    quit -> stop in postreader

--- frontend/PostReader.py
+++ frontend/PostReader.py
@@ -26,12 +26,12 @@ class PostReader(IPostReader):
         :return: None
         """
         print("Retrieving posts...")
-        print("Press Enter to fetch the next post. Type 'exit' or 'quit' to return to the DERP console.")
+        print("Press Enter to fetch the next post. Type 'exit' or 'stop' to return to the DERP console.")
         print("")
         for post in executor.results():
             print("Source: {0}".format(post.source().name()))
             for field_name in post.definition().field_definitions():
                 print("{0}: {1}".format(field_name, post.field_data(field_name)))
             cmd = input("")
-            if cmd.lower() == "exit" or cmd.lower() == "quit":
+            if cmd.lower() == "exit" or cmd.lower() == "stop":
                 break
