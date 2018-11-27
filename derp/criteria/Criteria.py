5d4c3aa631fb0b10ee26c2a3c4919252693e0267
Logan Lembke <logan.lembke@mines.sdsmt.edu>
2018-11-27 00:13:27 -0700

    Reimplement Criteria and Selection with append(line_ast) methods.

--- derp/criteria/Criteria.py
+++ derp/criteria/Criteria.py
@@ -7,66 +7,44 @@ class Criteria:
     Eagerly computes the qualifier tree for a list of criteria expressions.
     """
 
-    def __init__(self, criteria_ast_list):
-        self.expressions = []
-        for statement in criteria_ast_list:
-            add_remove_expr = statement.children[0]
-            if add_remove_expr.data == "add_expression":
-                self.expressions.append(AddCriteriaExpression(add_remove_expr.children[0]))
-            else:
-                self.expressions.append(RemoveCriteriaExpression(add_remove_expr.children[0]))
+    def __init__(self, criteria_ast_list=None):
+        if criteria_ast_list is None:
+            criteria_ast_list = []
+        self.__q_tree = None
+        for ast in criteria_ast_list:
+            self.append(ast)
 
-        self.__set_qualifier_tree()
-
-    def __set_qualifier_tree(self):
+    def append(self, criteria_ast):
         """
-        creates a qualifier tree representing the entire selection and stores it in self.__q_tree
+        Builds the underlying qualifier tree based off of the current state and the given criteria_ast.
+        Order matters when appending criteria asts to a given criteria.
+        :param criteria_ast: A Lark criteria ast which has been semantically validated
         """
-
-        # Insert an AndNode between two statements if the latter statement is a RemoveCriteriaExpression
-        # and an OrNode otherwise.
-
-        if isinstance(self.expressions[0], AddCriteriaExpression):
-            ret_tree = self.expressions[0].qualifier_tree()
-        else:
-            ret_tree = derp.qualifiers.NotNode(self.expressions[0].qualifier_tree())
-
-        for i in range(1, len(self.expressions)):
-            curr_expression = self.expressions[i]
-
-            if isinstance(curr_expression, AddCriteriaExpression):
-                curr_tree = curr_expression.qualifier_tree()
-            else:
-                curr_tree = derp.qualifiers.NotNode(curr_expression.qualifier_tree())
-
-            if isinstance(curr_expression, RemoveCriteriaExpression):
-                ret_tree = derp.qualifiers.AndNode(ret_tree, curr_tree)
-            else:
-                ret_tree = derp.qualifiers.OrNode(ret_tree, curr_tree)
-
-        self.__q_tree = ret_tree
-
-    def qualifier_tree(self):
-        return self.__q_tree
-
-
-class CriteriaExpression:
-    """
-    Represents a set of qualifiers specified in a criteria expression.
-    Eagerly computes the corresponding qualifier tree.
-    """
-
-    def __init__(self, selector):
+        add_remove_expression = criteria_ast.children[0]
+        is_add_expression = add_remove_expression.data == "add_expression"  # Otherwise remove expression
+        selector = add_remove_expression.children[0]
         root_qualifier = selector.children[0]
-        self.__q_tree = derp.qualifiers.LarkQualifierConverter.convert(root_qualifier)
+        expression_q_tree = derp.qualifiers.LarkQualifierConverter.convert(root_qualifier)
+
+        # Start out the criteria if it is empty
+        if self.__q_tree is None:
+            if is_add_expression:
+                self.__q_tree = expression_q_tree
+            else:
+                self.__q_tree = derp.qualifiers.NotNode(expression_q_tree)
+            return
+
+        if is_add_expression:
+            self.__q_tree = derp.qualifiers.OrNode(self.__q_tree, expression_q_tree)
+
+        else:
+            self.__q_tree = derp.qualifiers.AndNode(
+                self.__q_tree,
+                derp.qualifiers.NotNode(expression_q_tree)
+            )
 
     def qualifier_tree(self):
+        """
+        :return: a QualifierTree which represents the constraints of the Criteria
+        """
         return self.__q_tree
-
-
-class AddCriteriaExpression(CriteriaExpression):
-    pass
-
-
-class RemoveCriteriaExpression(CriteriaExpression):
-    pass
--- derp/selections/Selection.py
+++ derp/selections/Selection.py
@@ -22,82 +22,82 @@ class ASTKey:
 class Selection:
     """
     Represents a selection using a list of SelectionExpressions.
-    Builds SelectionExpressions from a list of parsed selection statements.
+    Builds SelectionExpressions from parsed selection statements.
     """
 
-    def __init__(self, selection_ast_list):
-        self.expressions = []
-        for statement in selection_ast_list:
-            add_remove_expr = statement.children[0]
-            if add_remove_expr.data == "add_expression":
-                self.expressions.append(
-                    AddSelectionExpression(add_remove_expr.children[0]))
-            else:
-                self.expressions.append(
-                    RemoveSelectionExpression(add_remove_expr.children[0]))
-        self.__set_source_ast_qualifier_tree_map()
+    def __init__(self, selection_ast_list=None):
+        if selection_ast_list is None:
+            selection_ast_list = []
 
-    def __set_source_ast_qualifier_tree_map(self):
-        """
-        Creates a mapping from the asts referenced in the selection to the qualifier
-        trees needing to be executed for each source ast.
-        Sets the result on self.__source_ast_qualifier_tree_map.
-        """
+        # mapping from the asts referenced in the selection to the qualifier
+        # trees needing to be executed for each source ast.
+        # Key: str(source_ast) Value: (source_ast, qualifier_tree)
+        # The ast must be stringified to use it as a dict key
         self.__source_ast_qualifier_tree_map = {}
 
-        # loop through all of the expressions in order to reduce the multiple expression
-        # qualifier trees into one per source ast
-        for expression in self.expressions:
+        for ast in selection_ast_list:
+            self.append(ast)
 
-            # Add expressions and remove expressions need different handlers
-            if isinstance(expression, AddSelectionExpression):
+    def append(self, selection_ast):
+        add_remove_expr = selection_ast.children[0]
+        # Add expressions and remove expressions need different handlers
+        if add_remove_expr.data == "add_expression":
+            self.__append_add_expression(AddSelectionExpression(add_remove_expr.children[0]))
+        else:
+            self.__append_remove_expression(RemoveSelectionExpression(add_remove_expr.children[0]))
 
-                # Add expressions may reference multiple module sources or selection sources
-                for source_ast in expression.source_asts():
-                    if source_ast.data == "source_module":
-                        source_ast_key = ASTKey(source_ast)
-                        # Module sources are easy, we just or the qualifier tree for the
-                        # expression with the existing tree for that source
-                        if source_ast_key in self.__source_ast_qualifier_tree_map:
-                            self.__source_ast_qualifier_tree_map[source_ast_key] = OrNode(
-                                self.__source_ast_qualifier_tree_map[source_ast_key],
+    def __append_add_expression(self, expression):
+        # Add expressions may reference multiple module sources or selection sources
+        for source_ast in expression.source_asts():
+            if source_ast.data == "source_module":
+                # Module sources are easy, we just or the qualifier tree for the
+                # expression with the existing tree for that source
+                source_ast_key = ASTKey(source_ast)
+
+                if source_ast_key in self.__source_ast_qualifier_tree_map:
+                    self.__source_ast_qualifier_tree_map[source_ast_key] = OrNode(
+                        self.__source_ast_qualifier_tree_map[source_ast_key],
+                        expression.qualifier_tree()
+                    )
+                else:
+                    self.__source_ast_qualifier_tree_map[source_ast_key] = expression.qualifier_tree()
+            elif source_ast.data == "source_selection":
+                # Selection sources are a bit more difficult.
+                # We need to get the source ast - qualifier tree map for the referenced
+                # selection, qualify each qualifier tree for each nested source with the
+                # qualifier tree specified in this expression and or each nested source's
+                # qualifier tree with the one in this selection's map
+                nested_selection = Selection(source_ast.children[0])
+                nested_source_ast_qualifier_tree_map = nested_selection.source_ast_qualifier_tree_map()
+                for nested_source_ast_key in nested_source_ast_qualifier_tree_map:
+                    if nested_source_ast_key in self.__source_ast_qualifier_tree_map:
+                        self.__source_ast_qualifier_tree_map[nested_source_ast_key] = OrNode(
+                            self.__source_ast_qualifier_tree_map[nested_source_ast_key],
+                            AndNode(
+                                nested_source_ast_qualifier_tree_map[nested_source_ast_key],
                                 expression.qualifier_tree()
                             )
-                        else:
-                            self.__source_ast_qualifier_tree_map[source_ast_key] = expression.qualifier_tree()
-                    elif source_ast.data == "source_selection":
-                        # Selection sources are a bit more difficult.
-                        # We need to get the source ast - qualifier tree map for the referenced
-                        # selection, qualify each qualifier tree for each nested source with the
-                        # qualifier tree specified in this expression and or each nested source's
-                        # qualifier tree with the one in this selection's map
-                        nested_selection = Selection(source_ast.children[0])
-                        nested_source_ast_qualifier_tree_map = nested_selection.source_ast_qualifier_tree_map()
-                        for nested_source_ast_key in nested_source_ast_qualifier_tree_map:
-                            if nested_source_ast_key in self.__source_ast_qualifier_tree_map:
-                                self.__source_ast_qualifier_tree_map[nested_source_ast_key] = OrNode(
-                                    self.__source_ast_qualifier_tree_map[nested_source_ast_key],
-                                    AndNode(
-                                        nested_source_ast_qualifier_tree_map[nested_source_ast_key],
-                                        expression.qualifier_tree()
-                                    )
-                                )
-                            else:
-                                self.__source_ast_qualifier_tree_map[nested_source_ast_key] = AndNode(
-                                    nested_source_ast_qualifier_tree_map[nested_source_ast_key],
-                                    expression.qualifier_tree()
-                                )
+                        )
+                    else:
+                        self.__source_ast_qualifier_tree_map[nested_source_ast_key] = AndNode(
+                            nested_source_ast_qualifier_tree_map[nested_source_ast_key],
+                            expression.qualifier_tree()
+                        )
 
-            elif isinstance(expression, RemoveSelectionExpression):
-                # NOTE: We could support "Remove from module_source" here
-                for source_ast_key in self.__source_ast_qualifier_tree_map:
-                    # each source must already have a corresponding tree
-                    # as REMOVE statements may only come after at least one
-                    # ADD statement
-                    self.__source_ast_qualifier_tree_map[source_ast_key] = AndNode(
-                        self.__source_ast_qualifier_tree_map[source_ast_key],
-                        NotNode(expression.qualifier_tree())
-                    )
+    def __append_remove_expression(self, expression):
+        # NOTE: We could support "Remove from module_source" here
+
+        # TODO Make this a proper exception
+        assert len(self.__source_ast_qualifier_tree_map) != 0
+
+        for source_ast_key in self.__source_ast_qualifier_tree_map:
+            # each source must already have a corresponding tree
+            # as REMOVE statements may only come after at least one
+            # ADD statement
+            self.__source_ast_qualifier_tree_map[source_ast_key] = AndNode(
+                self.__source_ast_qualifier_tree_map[source_ast_key],
+                NotNode(expression.qualifier_tree())
+            )
 
     def source_ast_qualifier_tree_map(self):
         return self.__source_ast_qualifier_tree_map
